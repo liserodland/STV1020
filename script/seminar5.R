749944bbf6c9335e954fab2c9eb6362c467a4ae5
Lise Rødland <liserod@uio.no>
2021-02-16 08:49:22 +0100

    Laster opp script

--- script/seminar5.R
+++ script/seminar5.R
@@ -1,4 +1,4 @@
-
+## ---- eval = TRUE, include=TRUE, echo=TRUE, message=FALSE----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Bestemmer working directory
 setwd("../../STV1020/")
 
@@ -9,6 +9,9 @@ library(stargazer)
 # Laster inn datasettet
 load("data/FairFPSR3.RData")
 
+
+
+## Undersøker data
 # Hent ut informasjon om variabelnavn, klasse m.m.
 str(FairFPSR3)
 
@@ -28,17 +31,8 @@ FairFPSR3 <- FairFPSR3 %>%
   mutate(complete = complete.cases(.),
          inf_na = is.na(inflation))
 
-  
-#  **SETT INN MER OM PLOTTING HER**
-#  (inkl. hvordan lagre plot)
 
-  ggplot(data = FairFPSR3) +
-  geom_point(aes(y = inc_vote, x=growth))+
-  theme_bw() +
-  theme(legend.title=element_blank()) +
-  ylab("Incumbent vote share") +
-  xlab("Growth rate")
-
+## Omkoding av variabler
 # Oppretter den nye variabelen og endrer referansekategori
 FairFPSR3 <- FairFPSR3 %>% 
   mutate(growth_dich = ifelse(growth > 0, "Growth", "No growth"),
@@ -48,25 +42,40 @@ FairFPSR3 <- FairFPSR3 %>%
 class(FairFPSR3$growth_dich)
 table(FairFPSR3$growth_dich, useNA = "always")
 
+# Eksempel på plott. Kan byttes ut med andre plott i f.eks. Healy 
+# (har ikke fått boka enda så vet ikke hva de vektlegger der)
 ggplot(data = FairFPSR3) +
-  geom_point(aes(x=growth, y = inc_vote, col = growth_dich)) +
+  geom_histogram(aes(x=growth, fill = growth_dich),
+               binwidth = 1) +
   theme_bw() +
   theme(legend.title=element_blank()) +
-  ylab("Incumbent vote share") +
-  xlab("Growth rate") 
+  xlab("Growth rate") +
+  ylab("No. of observations")
 
 
+
+## Litt plotting før regresjon
+ggplot(data = FairFPSR3) +
+  geom_point(aes(x = growth, y = inc_vote)) +
+  theme_classic() +
+  theme(axis.line = element_line(colour = "black"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  ylab("Incumbent-Party Vote Percentage") +
+  xlab("Percentage change in Real GDP Per Capita") 
+
+
+# Regresjon med numerisk uavhengig variabel 
 model <- lm(inc_vote ~ growth, 
             data = FairFPSR3,
+            na.action = "na.exclude")
 
-summary(model)
-
+ummary(model)
 stargazer(model, 
           type = "text")
 
-stargazer(model,  
+stargazer(model, 
           type = "html",
-          out = "output/model1_tab.htm")
+          out = "../output/model1_tab.htm")
 
 
 ggplot(data = FairFPSR3) +
@@ -76,6 +85,7 @@ ggplot(data = FairFPSR3) +
         panel.border = element_rect(colour = "black", fill=NA, size=1)) +
   ylab("Incumbent-Party Vote Percentage") +
   xlab("Percentage change in Real GDP Per Capita") 
+
 
 FairFPSR3 <- FairFPSR3 %>% 
   mutate(fitted = fitted(model), 
@@ -104,6 +114,7 @@ ggplot(data = FairFPSR3) +
 
 confint(model)
 
+## Regresjon med dikotom uavhengig variabel
 model_dich <- lm(inc_vote ~ growth_dich, data = FairFPSR3)
 stargazer(model_dich, 
           type = "text")
